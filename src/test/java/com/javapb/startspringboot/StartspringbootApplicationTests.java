24aac5af88681a4ccaf95451a32dca5bb07521a2
EndrioAlbeton <endrio.alberton@gmail.com>
2024-01-09 12:17:38 -0300

    Project created

---
+++ src/test/java/com/javapb/startspringboot/StartspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapb.startspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

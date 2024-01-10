3fe4f6b00810a672db9f9b0089302a1a9768c8bf
EndrioAlbeton <endrio.alberton@gmail.com>
2024-01-10 13:28:28 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/javapb/startspringboot/entities/Order.java
+++ src/main/java/com/javapb/startspringboot/entities/Order.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.javapb.startspringboot.entities.enums.OrderStatus;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +16,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -41,9 +39,6 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-
 	public Order() {
 		
 	}
@@ -89,14 +84,6 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
-	}
-
-	public Payment getPayment() {
-		return payment;
-	}
-
-	public void setPayment(Payment payment) {
-		this.payment = payment;
 	}
 	
 	public Set<OrderItem> getItems(){

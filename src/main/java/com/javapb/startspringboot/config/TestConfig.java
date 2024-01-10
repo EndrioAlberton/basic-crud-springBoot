7e52e5f0f591ba385a96b0b15cafd9d02891a9aa
EndrioAlbeton <endrio.alberton@gmail.com>
2024-01-10 10:23:02 -0300

    OrderStatus enum

--- src/main/java/com/javapb/startspringboot/config/TestConfig.java
+++ src/main/java/com/javapb/startspringboot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.javapb.startspringboot.entities.Order;
 import com.javapb.startspringboot.entities.User;
+import com.javapb.startspringboot.entities.enums.OrderStatus;
 import com.javapb.startspringboot.repositories.OrderRepository;
 import com.javapb.startspringboot.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Justin", "justin@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Eduardo", "edu@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATTING_PAYMENT,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, u1); 
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
